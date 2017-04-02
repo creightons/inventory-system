1ce03d89714ee393656dce82e0420463887f7810
a <creightons26@gmail.com>
2017-04-02 19:27:11 -0400

    Converted 'Customers' and 'Products' page to use the new table view

--- controllers/customers.js
+++ controllers/customers.js
@@ -1,10 +1,59 @@
 const router = require('express').Router(),
-	Customer = require('../models/customer');
+	Customer = require('../models/customer'),
+	{ paginationLimit } = require('../constants');
 
 router.get('/customers', function(req, res) {
-	Customer.find({}).select('companyName address').then(customers => {
-		return res.status(200).render('customers', { customers });
-	});
+	const pageNumber = parseInt(req.query.pageNumber) || 1;
+
+	const skipCount = (pageNumber - 1) * paginationLimit;
+
+	// Grab an extra document (1 more than limit) to check whether there are 
+	// any more documents to grab after this page of results
+	const limitCount = paginationLimit + 1;
+
+	Customer
+		.find({})
+		.select('companyName address')
+		.skip(skipCount)
+		.limit(limitCount)
+		.exec()
+		.then(customers => {
+			let nextButtonUrl,
+				previousButtonUrl;
+			
+			const nextButtonEnabled = customers.length === limitCount;
+			const previousButtonEnabled = pageNumber !== 1;
+
+			if (nextButtonEnabled) {
+				const nextPage = pageNumber + 1;
+				nextButtonUrl = `/customers?pageNumber=${nextPage}`;
+			}
+
+			if (previousButtonEnabled) {
+				const prevPage = pageNumber - 1;
+				previousButtonUrl = `/customers?pageNumber=${prevPage}`;
+			}
+
+			const columnTitles = [ 'Name', 'Address' ];
+
+			const rows = customers.map(customer => {
+				return [
+					customer.companyName,
+					customer.address
+				];
+			});
+
+			return res.status(200).render('customers', {
+				table: {
+					nextButtonEnabled,
+					previousButtonEnabled,
+					nextButtonUrl,
+					previousButtonUrl,
+					columnTitles,
+					rows,
+				},
+			});
+		});
 });
 
 module.exports = router;
--- controllers/products.js
+++ controllers/products.js
@@ -1,12 +1,62 @@
 const router = require('express').Router(),
-	Product = require('../models/product');
+	Product = require('../models/product'),
+	{ paginationLimit } = require('../constants');
 
 router.get('/products', function(req, res) {
-	Product.find({}).select('name price vendor productId').then(products => {
-		return res.status(200).render('products', {
-			products,
+	const pageNumber = parseInt(req.query.pageNumber) || 1;
+
+	const skipCount = (pageNumber - 1) * paginationLimit;
+
+	// Grab an extra document (1 more than limit) to check whether there are 
+	// any more documents to grab after this page of results
+	const limitCount = paginationLimit + 1;
+
+	Product
+		.find({})
+		.select('name price vendor productId')
+		.skip(skipCount)
+		.limit(limitCount)
+		.exec()
+		.then(products => {
+
+			let nextButtonUrl,
+				previousButtonUrl;
+			
+			const nextButtonEnabled = products.length === limitCount;
+			const previousButtonEnabled = pageNumber !== 1;
+
+			if (nextButtonEnabled) {
+				const nextPage = pageNumber + 1;
+				nextButtonUrl = `/products?pageNumber=${nextPage}`;
+			}
+
+			if (previousButtonEnabled) {
+				const prevPage = pageNumber - 1;
+				previousButtonUrl = `/products?pageNumber=${prevPage}`;
+			}
+
+			const columnTitles = [ 'Name', 'Price', 'Vendor', 'Product Id' ];
+
+			const rows = products.map(product => {
+				return [
+					product.name,
+					'$' + product.price,
+					product.vendor,
+					product.productId,
+				];
+			});
+
+			return res.status(200).render('products', {
+				table: {
+					nextButtonEnabled,
+					previousButtonEnabled,
+					nextButtonUrl,
+					previousButtonUrl,
+					columnTitles,
+					rows,
+				},
+			});
 		});
-	});
 });
 
 module.exports = router;
