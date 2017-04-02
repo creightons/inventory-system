20b62fa198d93a71fc5e1c9613f0e8ce648297c8
a <creightons26@gmail.com>
2017-04-02 19:03:48 -0400

    Created a paginated table (broke other tables in process; they must be updated)

---
+++ constants.js
@@ -0,0 +1,3 @@
+module.exports = {
+	paginationLimit: 10,
+};
--- controllers/orders.js
+++ controllers/orders.js
@@ -1,15 +1,58 @@
 const router = require('express').Router(),
-	Order = require('../models/order');
+	Order = require('../models/order'),
+	{ paginationLimit } = require('../constants');
 
 router.get('/orders', function(req, res) {
+	const pageNumber = parseInt(req.query.pageNumber) || 1;
+
+	const skipCount = (pageNumber - 1) * paginationLimit;
+
+	// Grab an extra document (1 more than limit) to check whether there are 
+	// any more documents to grab after this page of results
+	const limitCount = paginationLimit + 1;
+
 	Order.find({})
 		.select('quantity customer product')
 		.populate('customer', 'companyName')
 		.populate('product', 'name')
-		.limit(30)
+		.skip(skipCount)
+		.limit(limitCount)
 		.exec()
 		.then(orders => {
-			return res.status(200).render('orders', { orders });
+			let nextButtonUrl,
+				previousButtonUrl;
+			
+			const nextButtonEnabled = orders.length === limitCount;
+			const previousButtonEnabled = pageNumber !== 1;
+
+			if (nextButtonEnabled) {
+				const nextPage = pageNumber + 1;
+				nextButtonUrl = `/orders?pageNumber=${nextPage}`;
+			}
+
+			if (previousButtonEnabled) {
+				const prevPage = pageNumber - 1;
+				previousButtonUrl = `/orders?pageNumber=${prevPage}`;
+			}
+
+			const columnTitles = [ 'Product', 'Quantity', 'Customer' ];
+			const rows = orders.map(order => {
+				return [
+					order.product.name,
+					order.quantity,
+					order.customer.companyName,
+				];
+			});
+
+
+			return res.status(200).render('orders', {
+				nextButtonEnabled,
+				previousButtonEnabled,
+				nextButtonUrl,
+				previousButtonUrl,
+				columnTitles,
+				rows,
+			});
 		})
 		.catch(err => {
 			console.log('err', err);
