420858d79f776c0819956fba7aae99b73d6bff86
a <creightons26@gmail.com>
2017-04-04 23:33:05 -0400

    Added route to process added customers as well as handle errors

--- controllers/customers.js
+++ controllers/customers.js
@@ -34,4 +34,43 @@ router.get('/customers/add', (req, res) => {
 	res.status(200).render('customers-add');
 });
 
+const existingCustomerError = new Error('The customer already exists.');
+const missingNameError = new Error('The customer must have a name.');
+const missingAddressError = new Error('The customer must have an address.');
+
+router.post('/customers/add', (req, res) => {
+	const {	companyName, address } = req.body;
+
+	if (!companyName) {
+		return respondWithError({ res, status: 400, err: missingNameError });
+	}
+	
+	if (!address) {
+		return respondWithError({ res, status: 400, err: missingAddressError });
+	}
+
+	Customer
+		.findOne({ companyName })
+		.then(customer => {
+			if (customer) { throw existingCustomerError; }
+
+			const newCustomer = Customer({ companyName, address });
+			return newCustomer.save();
+		})
+		.then(() => {
+			res.status(200).redirect('/customers');
+		})
+		.catch(existingCustomerError, err => {
+			respondWithError({ res, status: 400, err });
+		})
+		.catch( err => respondWithError({ res, err }) );
+});
+
+function respondWithError({ res, status, err }) {
+	const responseStatus = status || 500;
+	res.status(responseStatus).render('customers-add', {
+		error: err.message,
+	});
+}
+
 module.exports = router;
