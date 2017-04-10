e1b68ca174de6cc9012b1e27d99447608c0f7719
a <creightons26@gmail.com>
2017-04-10 18:44:10 -0400

    Added a page to add products; Fixed a bug where too many results were shown on a table page

--- controllers/products.js
+++ controllers/products.js
@@ -36,4 +36,47 @@ router.get('/products/add', (req, res) => {
 	res.status(200).render('products-add');
 });
 
+const missingNameError = new Error('The product must have a name.');
+const missingPriceError = new Error('The product must have a price.');
+const missingVendorError = new Error('The product must have a vendor.');
+const missingDescriptionError = new Error('The product must have a description.');
+const missingProductIdError = new Error('The product must have an ID number.');
+const existingProductError = new Error('The product already exists.');
+
+router.post('/products/add', (req, res) => {
+	const { name, price, vendor, productId, description } = req.body;
+
+	if (!name) { return respondWithError({ res, status: 400, err: missingNameError }); }
+	if (!price) { return respondWithError({ res, status: 400, err: missingPriceError }); }
+	if (!vendor) { return respondWithError({ res, status: 400, err: missingVendorError }); }
+	if (!productId) { return respondWithError({ res, status: 400, err: missingProductIdError }); }
+	if (!description) { return respondWithError({ res, status: 400, err: missingDescriptionError }); }
+
+	Product
+		.findOne({ name })
+		.then(product => {
+			if(product) { throw existingProductError; }
+
+			const newProduct = Product({ name, price, vendor, productId, description });
+			return newProduct.save();
+		})
+		.then(() => {
+			res.status(200).redirect('/products');
+		})
+		.catch(existingProductError, err => {
+			respondWithError({ res, status: 400, err });
+		})
+		.catch(
+			err => respondWithError({ res, err })
+		);
+});
+
+function respondWithError({ res, status, err }) {
+	const responseStatus = status || 500;
+	console.log(err);
+	res.status(responseStatus).render('products-add', {
+		error: err.message,
+	});
+}
+
 module.exports = router;
